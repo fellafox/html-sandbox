0882fbdf093de53a502c078a5ca3be3b645e6598
Emily Halleran <emily@blueroserevival.com>
2017-04-11 16:42:13 -0600

    Test 1

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>
+
+		</title>
+		<meta charset="utf-8"/>
+		<title>Basic HTML Page</title>
+	</head>
+	<body>
+		<h1>Hello, World!! :D</h1>
+	</body>
+</html>
