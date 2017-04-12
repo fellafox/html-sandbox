742ccbec77ed65ae2b8eb0bea9036d473e8871ab
Emily Halleran <emily@blueroserevival.com>
2017-04-12 08:49:54 -0600

    Added Image

--- index.php
+++ index.php
@@ -16,6 +16,9 @@
 		<main>
 			<h2>Deanna Troi says:</h2>
 			<p>Sharing an orbit with God is no small experience</p>
+			<p>To admit that you're afraid gives you strength.</p>
+			<p>You are given time to choose the path. If you wait too long the path chooses you. - Emily 4.12.2017</p>
+<img src="images/nebula.jpg"/>
 		</main>
 	</body>
 </html>
