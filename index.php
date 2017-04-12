df6c0fa4a4c44aeec8e1c8d772c1d4152a79b966
Emily Halleran <emily@blueroserevival.com>
2017-04-12 08:19:33 -0600

    Test 1

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-		<link rel="stylesheet" href="css/style.css"/>
+<!-- Custom CSS  <-->
+		<link rel="stylesheet" href="css/style.css" type="text/css"/>
 		<title>HTML SANDBOX</title>
 	</head>
 	<body>
